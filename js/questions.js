813f74c50642fe43456007eb7bc0fa5fe4a078e0
tyhaggis86 <113810488+tyhaggis86@users.noreply.github.com>
2022-12-11 14:30:01 +1000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which one of Santa's reindeer has the same name as another holiday mascot??",
+    answer: "Cupid",
+    options: [
+      "Prancer",
+      "Cupid",
+      "Comet",
+      "Donner"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many ghosts show up in A Christmas Carol?",
+    answer: "4",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The movie Miracle on 34th Street is based on a real-life department store?",
+    answer: "Macys",
+    options: [
+      "Macys",
+      "Bloomingdales",
+      "Saks Fifth Avenue",
+      "Westfield"
+    ]
+  },
+    {
+    numb: 4,
+    question: "In Home Alone, where are the McCallisters going on vacation when they leave Kevin behind?",
+    answer: "Paris",
+    options: [
+      "Rome",
+      "New York",
+      "Paris",
+      "London"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What colour Christmas is Elvis going to have?",
+    answer: "Blue",
+    options: [
+      "White",
+      "Red",
+      "Blue",
+      "Green"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which Hollywood actor played six different roles in The Polar Express",
+    answer: "Tom Hanks",
+    options: [
+      "Tom Hanks",
+      "Richard Attenborough",
+      "Tim Allen",
+      "Bruce Willis"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What are you supposed to do when you find yourself under the mistletoe?",
+    answer: "Kiss",
+    options: [
+      "Dance",
+      "Sing",
+      "Kiss",
+      "Run away"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which real-life person is Santa Claus based on?",
+    answer: "St. Nicholas",
+    options: [
+      "St. Christopher",
+      "St. Bernard",
+      "St. Jude",
+      "St. Nicholas"
+    ]
+  },
+  {
+    numb: 9,
+    question: "In the movie Elf, what was the first rule of The Code of Elves?",
+    answer: "Treat everyday like Christmas",
+    options: [
+      "Treat everyday like Christmas",
+      "Check the list twice",
+      "There is room for everyone on the nice list",
+      "The BEST way to spread Christmas cheer is singing loud for all to hear"
+    ]
+  },
+  {
+    numb: 10,
+    question: "How do you say Merry Christmas in Spanish?",
+    answer: "Feliz Navidad",
+    options: [
+      "Nollaig Shona",
+      "Feliz Navidad",
+      "Marau na Kerisimasi",
+      "Meri Kirihimete"
+    ]
+  },
+  {
+    numb: 11,
+    question: "What is the best-selling Christmas song ever?",
+    answer: "White Christmas by Bing Crosby",
+    options: [
+      "All I Want for Christmas Is You by Mariah Carey",
+      "Do They Know It's Christmas by Band Aid",
+      "Fairytale of New York by The Pogues",
+      "White Christmas by Bing Crosby"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What was Frosty the Snowman's nose made out?",
+    answer: "A button",
+    options: [
+      "Corn cob pipe",
+      "A button",
+      "Coal",
+      "A carrot"
+    ]
+  },
+  {
+    numb: 13,
+    question: "What popular Christmas song was actually written for Thanksgiving?",
+    answer: "Jingle Bells",
+    options: [
+      "Jingle Bells",
+      "Deck the Halls",
+      "Joy to the World",
+      "O Holy Night"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What was the first company that used Santa Claus in advertising?",
+    answer: "Coca Cola",
+    options: [
+      "Pepsi",
+      "Dr. Pepper",
+      "Coca Cola",
+      "Mountain Dew"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What is The Grinches dog's name?",
+    answer: "Max",
+    options: [
+      "Fido",
+      "Charlie",
+      "Buddy",
+      "Max"
+    ]
+  },
+];
